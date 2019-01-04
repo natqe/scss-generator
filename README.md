d51f3f15b4de746c65851728dbb0f2a5b374e31c
Natan Farkash <natqe3@gmail.com>
2019-01-04 12:22:45 +0200

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# scss-generator  VSCode Extension
+
+This extension will add snippet to your scss files when you have html file with the same name.
+The extension use your html template to create snippet with initial scss boilerplate.
+<!-- ![Example Image](https://file-wkbcnlcvbn.now.sh/import-cost.gif) -->
+
+## Known Issues
+
+none.
+
+### 1.0.0
+
+Initial release of scss generator
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,96 @@
+import { JSDOM } from 'jsdom'
+import { invoke, repeat, values } from 'lodash'
+import { prefix } from 'utilizes/prefix'
+import * as vscode from 'vscode'
+
+const { CompletionItem, languages, window, workspace } = vscode
+
+export function activate(context: vscode.ExtensionContext) {
+
+    context.subscriptions.push(languages.registerCompletionItemProvider(
+        `scss`,
+        {
+            provideCompletionItems(document, position, token, context) {
+
+                const
+                    titleOf = (fileName: string) => {
+
+                        let result = fileName.split(fileName.includes(`/`) ? `/` : `\\`).pop()
+
+                        if (result) {
+
+                            if (result.startsWith(`_`)) result = result.slice(1)
+
+                            const lastIndexDot = result.lastIndexOf(`.`)
+
+                            if (lastIndexDot > 0) result = result.slice(0, lastIndexDot)
+
+                            return result
+
+                        }
+
+                    },
+                    html = invoke(workspace.textDocuments.find(({ fileName, languageId }) => languageId === `html` && titleOf(fileName) === titleOf(document.fileName)), `getText`)
+
+                if (html) {
+
+                    let scss = ``
+
+                    const append = (current: Element, count = 0) => {
+
+                        let indexInParent = 0
+
+                        const
+                            { parentElement } = current,
+                            siblings: Array<Element> = values(parentElement.children).filter((sibling, index) => {
+                                if (sibling !== current) return true
+                                else indexInParent = index
+                            }),
+                            createBaseSelectors = ({ localName, classList, id }: Element) => `${localName}${prefix(`#`, id) || values(classList).map(token => `.` + token).join(``)}`
+
+                        let selectors = createBaseSelectors(current)
+
+                        if (parentElement.childElementCount > 1 && siblings.some(sibling => createBaseSelectors(sibling) === selectors)) selectors += `:nth-of-type(${indexInParent + 1})`
+
+                        scss += selectors
+
+                        scss += ` {`
+
+                        const start = `\n${repeat(`  `, ++count)}`
+
+                        for (const element of values(current.children)) {
+
+                            scss += `${start}>`
+
+                            append(element, count)
+
+                        }
+
+                        scss += `\n${repeat(`  `, --count)}}`
+
+                    }
+
+                    for (const element of values(new JSDOM(html).window.document.body.children)) append(element)
+
+                    let snippet = new CompletionItem(`scss boilerplate for current file`, 14)
+
+                    snippet.insertText = scss
+
+                    snippet.
+                        //@ts-ignore
+                        keepWhitespace = true
+
+                    snippet.preselect = true
+
+                    return [snippet]
+
+                }
+
+            }
+        }
+    ))
+
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
